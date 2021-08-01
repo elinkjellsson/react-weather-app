157e971720de924927b2dacf7845d699529520ed
Elin Kjellsson <elinkjellsson@me.com>
2021-08-01 11:46:21 +0200

    added link to github

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     Weather App
+     <h1>React Weather App</h1>
+     <footer>
+      <a href="https://github.com/elinkjellsson/react-weather-app" target="_blank" rel="noreferrer">Open-source code {" "}</a> 
+      by Elin Kjellsson
+      </footer>
     </div>
   );
 }
