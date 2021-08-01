01dc318cb5e5db184fdff72e7169dfc3a59d247b
Elin Kjellsson <elinkjellsson@me.com>
2021-08-01 12:39:59 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
      <h1>React Weather App</h1>
      <footer>
       <a href="https://github.com/elinkjellsson/react-weather-app" target="_blank" rel="noreferrer">Open-source code {" "}</a> 
-      by Elin Kjellsson
+      by Elin Kjellsson 
       </footer>
+      </div>
     </div>
   );
 }
